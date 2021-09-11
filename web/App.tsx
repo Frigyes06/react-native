ba75b64132a8b63df79da30157fb5a2d934dc9d2
Christopher David <chris@arcade.city>
2021-09-11 10:24:23 -0500

    Pull in App.tsx code from ignite

--- web/App.tsx
+++ web/App.tsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { RootStore, setupRootStore } from 'store/mst'
+import { RootStore, RootStoreProvider, setupRootStore } from 'store/mst'
 
 const App = () => {
   const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined)
+
+  const {
+    initialNavigationState,
+    onNavigationStateChange,
+    isRestored: isNavigationStateRestored,
+  } = useNavigationPersistence(storage, NAVIGATION_PERSISTENCE_KEY)
 
   // Kick off initial async loading actions, like loading fonts and RootStore
   useEffect(() => {
@@ -11,7 +17,20 @@ const App = () => {
     })()
   }, [])
 
-  return <h1>Zion</h1>
+  // Before we show the app, we have to wait for our state to be ready.
+  // In the meantime, don't render anything. This will be the background
+  // color set in native by rootView's background color.
+  // In iOS: application:didFinishLaunchingWithOptions:
+  // In Android: https://stackoverflow.com/a/45838109/204044
+  // You can replace with your own loading component if you wish.
+  if (!rootStore || !isNavigationStateRestored) return null
+
+  // otherwise, we're ready to render the app
+  return (
+    <RootStoreProvider value={rootStore}>
+      <AppNavigator initialState={initialNavigationState} onStateChange={onNavigationStateChange} />
+    </RootStoreProvider>
+  )
 }
 
 export default App
