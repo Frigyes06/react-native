a3cb0a86da2a80b8a092a2ab7d17d3d0fa76930c
Christopher David <chris@arcade.city>
2021-09-11 09:25:29 -0500

    Add path aliases

--- config-overrides.js
+++ config-overrides.js
@@ -1,24 +1,37 @@
-const { 
-    override, 
-    babelInclude, 
-    addDecoratorsLegacy, 
-    disableEsLint, 
-    removeModuleScopePlugin, 
-    addWebpackExternals 
-} = require("customize-cra");
-const path = require('path');
+const {
+  override,
+  babelInclude,
+  addDecoratorsLegacy,
+  disableEsLint,
+  removeModuleScopePlugin,
+  addWebpackExternals,
+  addWebpackAlias,
+} = require('customize-cra')
+const path = require('path')
 
 module.exports = override(
-    removeModuleScopePlugin(),
-    addDecoratorsLegacy(),
-    disableEsLint(),
-    babelInclude([
-        path.resolve("src"),
-        path.resolve("web")
-    ]),
-    addWebpackExternals({
-        realm:'RealmStub'
-    })
+  removeModuleScopePlugin(),
+  addDecoratorsLegacy(),
+  disableEsLint(),
+  babelInclude([path.resolve('src'), path.resolve('web')]),
+  addWebpackExternals({
+    realm: 'RealmStub',
+  }),
+  addWebpackAlias({
+    ['animations']: path.resolve(__dirname, './src/animations'),
+    ['api']: path.resolve(__dirname, './src/api'),
+    ['assets']: path.resolve(__dirname, './src/assets'),
+    ['components']: path.resolve(__dirname, './src/components'),
+    ['config']: path.resolve(__dirname, './src/config'),
+    ['crypto']: path.resolve(__dirname, './src/crypto'),
+    ['hooks']: path.resolve(__dirname, './src/hooks'),
+    ['lib']: path.resolve(__dirname, './src/lib'),
+    ['realm']: path.resolve(__dirname, './src/realm'),
+    ['services']: path.resolve(__dirname, './src/services'),
+    ['store']: path.resolve(__dirname, './src/store'),
+    ['theme']: path.resolve(__dirname, './src/theme'),
+    ['types']: path.resolve(__dirname, './src/types'),
+  })
 )
 
 // const path = require('path');
@@ -30,7 +43,7 @@ module.exports = override(
 //         disableEsLint(),
 //         removeModuleScopePlugin(),
 //     ),
-//     paths: function (paths, env) {        
+//     paths: function (paths, env) {
 //         paths.appIndexJs = path.resolve(__dirname, 'web/index.tsx');
 //         paths.appSrc = path.resolve(__dirname, 'web');
 //         return paths;
