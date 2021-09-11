b3bb2eac37610bc8ea44968c4fdeb5a8b73e630b
Christopher David <chris@arcade.city>
2021-09-11 11:07:38 -0500

    Theme from ignite

---
+++ src/theme/palette.ts
@@ -0,0 +1,21 @@
+export const palette = {
+  lightGrey: '#f5f6f8',
+  orange: '#f0665b',
+  orangeSecondary: '#e45e53',
+  purple: '#6289FD',
+
+  white: '#ffffff',
+  bg: '#1a242f',
+  primary: '#6089ff',
+  secondary: '#48c998',
+  deep: '#141d27',
+  extraDeep: '#222e3a',
+  highlight: '#3d6189',
+  disabledPrimary: '#4c6bc0',
+  greyText: '#6a798c',
+  active: '#48c998',
+  lightGreen: '#76d7b2',
+  border: '#1a242f',
+  iconGrey: '#44505d',
+  notMeMessage: '#232f39',
+}
