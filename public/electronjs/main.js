f93b9ab934ffd446ef25597a9e02ef7c6291fb3d
Christopher David <chris@arcade.city>
2021-09-13 05:30:08 -0500

    Update electron security/branding

--- public/electronjs/main.js
+++ public/electronjs/main.js
@@ -19,7 +19,7 @@ app.dirname = __dirname
 const path = require('path')
 const url = require('url')
 
-const iconPath = path.join(__dirname, '..', 'static', 'icon.png')
+const iconPath = path.join(__dirname, '..', 'static', 'zion-dark-theme.png')
 const appIcon = nativeImage.createFromPath(iconPath)
 
 let mainWindow
@@ -31,7 +31,7 @@ deeplink.on('received', (link) => {
   mainWindow.webContents.send('deeplink', link)
 })
 
-// app.setName('Sphinx Chat');
+app.setName('Zion')
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -41,6 +41,9 @@ function createWindow() {
       nodeIntegration: true,
       webviewTag: true,
       enableRemoteModule: true,
+      // https://stackoverflow.com/questions/63427191/security-warning-in-the-console-of-browserwindow-electron-9-2-0
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
     },
   })
 
@@ -75,10 +78,10 @@ function createWindow() {
 
   let menu = defaultMenu(app, shell)
   ;(menu[0].submenu[0] = {
-    label: 'About Sphinx',
+    label: 'About Zion',
     click: () => {
       dialog.showMessageBox({
-        message: 'Sphinx Chat \n\nVersion: ' + VERSION,
+        message: 'Zion \n\nVersion: ' + VERSION,
         buttons: ['OK'],
         icon: appIcon,
       })
