f391d9593afd51dda8b36e819ced202b87a68a27
Christopher David <chris@arcade.city>
2021-09-14 10:24:42 -0500

    log the chats and parsedChats, dont set yet

--- web/stores/chats-store/chats-actions/getChats.ts
+++ web/stores/chats-store/chats-actions/getChats.ts
@@ -5,6 +5,11 @@ export const getChats = async (self: ChatsStore) => {
   const chats = await relay.get('chats')
   if (!(chats && chats.length)) return false
   const parsedChats = chats.map((c) => self.parseChat(c))
-  self.setChats(parsedChats)
+  console.tron.display({
+    name: 'getChats',
+    preview: `Got ${chats.length} chats`,
+    value: { chats, parsedChats },
+  })
+  // self.setChats(parsedChats)
   return true
 }
